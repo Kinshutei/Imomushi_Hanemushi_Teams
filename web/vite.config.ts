bf7c6deb3a2f4dcf48993b14012f8440de94705e
Kinshutei <whitelily.goldeneagle.inn@gmail.com>
2026-03-11 23:00:21 +0900

    Update vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // ★ GitHubリポジトリ名に合わせて変更してください
 // 例: リポジトリが https://github.com/yourname/kisaki-db なら '/kisaki-db/'
-const REPO_NAME = '/kisaki-db/'
+const REPO_NAME = '/Imomushi_Hanemushi_Teams/'
 
 export default defineConfig({
   plugins: [react()],
